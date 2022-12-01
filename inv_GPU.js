3ed69d9cd85346bc006e1a35cc053ed624089c23
Talha-Rauf <bsee16024@itu.edu.pk>
2022-12-01 12:39:36 +0500

    Configuring CSS

--- inv_GPU.js
+++ inv_GPU.js
@@ -4,14 +4,15 @@ var http = require('http');
 var path = require("path");
 const app = express();
 
-
 app.set('view engine', 'ejs');
+
+app.use(express.static(__dirname + '/views'));
 
 app.use(express.json());
 const PORT = process.env.PORT || 3000;
 //Environment vairable => process.env.PORT is default value
 
-app.get('/', function(req,res){
+app.get('/add', function(req,res){
   res.render('pages/inv_GPU.ejs');
 });
 
